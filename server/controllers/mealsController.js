ce0590d50b018ef5a87ae7140bbaf3015d93c4e7
gary <gsb.gurpreet@gmail.com>
2020-11-17 09:02:17 -0500

    Add meals route

---
+++ server/controllers/mealsController.js
@@ -0,0 +1,85 @@
+const connection = require("../dbConnection");
+const mealSchema = require("../models/meal");
+
+const Meal = connection.model("Meal", mealSchema);
+
+const find = async (id) => {
+    const { _doc } = await Meal.findById(id);
+    return _doc;
+};
+
+// create
+const create = async ({
+    title,
+    picURL,
+    price,
+    servingSize,
+    servingType,
+    cuisineType,
+    ingredients,
+    requirements,
+    createdAt,
+    updatedAt,
+    chefId,
+}) => {
+    const { _doc } =
+        (await Meal.create({
+            title,
+            picURL,
+            price,
+            servingSize,
+            servingType,
+            cuisineType,
+            ingredients,
+            requirements,
+            createdAt,
+            updatedAt,
+            chefId,
+        })) || {};
+    return _doc;
+};
+
+// update
+const update = async ({
+    id,
+    title,
+    picURL,
+    price,
+    servingSize,
+    servingType,
+    cuisineType,
+    ingredients,
+    requirements,
+    createdAt,
+    updatedAt,
+    chefId,
+}) => {
+    const meal = await Meal.findById(id);
+
+    if (title) meal.title;
+    if (picURL) meal.picURL;
+    if (price) meal.price;
+    if (servingSize) meal.servingSize;
+    if (servingType) meal.servingType;
+    if (cuisineType) meal.cuisineType;
+    if (ingredients) meal.ingredients;
+    if (requirements) meal.requirements;
+    if (createdAt) meal.createdAt;
+    if (updatedAt) meal.updatedAt;
+    if (chefId) meal.chefId;
+
+    const { _doc } = await meal.save();
+    return _doc;
+};
+
+// delete
+const remove = async (id) => {
+    return await Meal.findByIdAndDelete(id);
+};
+
+module.exports = {
+    find,
+    create,
+    update,
+    remove,
+};
--- server/routes/meals.js
+++ server/routes/meals.js
@@ -1,7 +1,18 @@
 // ROUTER FOR OUR MEALS
 
 const router = require("express").Router();
-const mealController = require("../controllers/mealController");
+const mealController = require("../controllers/mealsController");
+
+router.get("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const meal = await mealController.find(id);
+        return meal;
+    } catch (errors) {
+        console.error(error);
+        res.status(500).json({ errors: ["Unexpected error occured"] });
+    }
+});
 
 router.post("/", async (req, res) => {
     try {
@@ -61,13 +72,69 @@ router.post("/", async (req, res) => {
             ichefId,
         });
 
-        res.json(meal)
+        res.json(meal);
     } catch (error) {
         console.error(error);
         res.status(500).json({ errors: ["Unexpected error occured"] });
     }
 });
 
-router.get("/", (req, res) => {
-    const { id } = req.user;
+router.put("/", async (req, res) => {
+    try {
+        const {
+            title,
+            picURL,
+            price,
+            servingSize,
+            servingType,
+            cuisineType,
+            ingredients,
+            requirements,
+            chefId,
+        } = req.body;
+
+        const errors = [];
+
+        if (title && typeof title !== "string")
+            errors.push("Invalid title type");
+        if (picURL && typeof picURL !== "string")
+            errors.push("Invalid picURL type");
+        if (price && Number.isFinite(price)) errors.push("Invalid price type");
+        if (servingSize && typeof servingSize !== "string")
+            errors.push("Invalid servingSize type");
+        if (servingType && typeof servingType !== "string")
+            errors.push("Invalid servingType type");
+        if (cuisineType && Array.isArray(cuisineType))
+            errors.push("Invlalid cuisineType type");
+        if (ingredients && typeof ingredients !== "string")
+            errors.push("Invalid ingredients type");
+        if (requirements && typeof requirements !== "string")
+            errors.push("Invalid requirements type");
+        if (chefId && typeof chefId !== "string")
+            errors.push("Invalid requirements type");
+
+        if (errors.length > 0) {
+            res.status(400).json({ errors });
+            return;
+        }
+
+        const meal = await mealController.update({
+            title,
+            picURL,
+            price,
+            servingSize,
+            servingType,
+            cuisineType,
+            ingredients,
+            requirements,
+            ichefId,
+        });
+
+        res.json(meal);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ errors: ["Unexpected error occured"] });
+    }
 });
+
+module.exports = router;
