01656d59e8026fdd4ef21c0f0dd51f08fc90c54c
gary <gsb.gurpreet@gmail.com>
2020-11-20 11:24:28 -0500

    Make meals router DRYer by using validationMiddleware

--- server/routes/meals.js
+++ server/routes/meals.js
@@ -34,39 +34,6 @@ router.post(
         if (!servingSize) errors.push("Missing servingSize");
         if (!cuisineType) errors.push("Missing cuisineType");
         if (!ingredients) errors.push("Missing ingredients");
-        // if no chefId is provided, use the user's chef profile
-        if (!chefId) {
-            const userId = req.user.id;
-            const { _id } = await chefsController.findOneWithUserId(userId);
-            chefId = String(_id);
-        }
-
-        if (title && typeof title !== "string")
-            errors.push("Invalid title type");
-        if (picURL && typeof picURL !== "string")
-            errors.push("Invalid picURL type");
-        if (price && Number.isFinite(price))
-            errors.push("Invalid price type");
-        if (servingSize && typeof servingSize !== "string")
-            errors.push("Invalid servingSize type");
-        if (servingType && typeof servingType !== "string")
-            errors.push("Invalid servingType type");
-        if (cuisineType) {
-            cuisineType = JSON.parse(cuisineType);
-            if (!isArrayOfStrings(cuisineType))
-                errors.push("Invlalid cuisineType type");
-        }
-        if (ingredients && typeof ingredients !== "string")
-            errors.push("Invalid ingredients type");
-        if (requirements && typeof requirements !== "string")
-            errors.push("Invalid requirements type");
-        if (typeof chefId !== "string")
-            errors.push("Invalid chefId type");
-
-        if (errors.length > 0) {
-            res.status(400).json({ errors });
-            return;
-        }
 
         const meal = await mealController.create({
             title,
@@ -99,37 +66,6 @@ router.put(
         } = req.body;
         let { chefId, cuisineType } = req.body;
 
-        const errors = [];
-        if (title && typeof title !== "string")
-            errors.push("Invalid title type");
-        if (picURL && typeof picURL !== "string")
-            errors.push("Invalid picURL type");
-        if (price && Number.isFinite(price))
-            errors.push("Invalid price type");
-        if (servingSize && typeof servingSize !== "string")
-            errors.push("Invalid servingSize type");
-        if (servingType && typeof servingType !== "string")
-            errors.push("Invalid servingType type");
-        if (cuisineType) {
-            cuisineType = JSON.parse(cuisineType);
-            if (!isArrayOfStrings(cuisineType))
-                errors.push("Invlalid cuisineType type");
-        }
-        if (ingredients && typeof ingredients !== "string")
-            errors.push("Invalid ingredients type");
-        if (requirements && typeof requirements !== "string")
-            errors.push("Invalid requirements type");
-        // if no chefId is provided, use the user's chef profile
-        if (!chefId) {
-            const userId = req.user.id;
-            const { _id } = await chefsController.findOneWithUserId(userId);
-            chefId = String(_id);
-        }
-        if (errors.length > 0) {
-            res.status(400).json({ errors });
-            return;
-        }
-
         const meal = await mealController.findOneWithId(id);
         if (!meal) {
             res.status(400).json({ errors: ["Meal for id not found"] });
@@ -152,5 +88,51 @@ router.put(
         res.json(newMeal);
     })
 );
+
+function validationMiddleware(req, res, next) {
+    const {
+        title,
+        picURL,
+        price,
+        servingSize,
+        servingType,
+        ingredients,
+        requirements,
+    } = req.body;
+    let { chefId, cuisineType } = req.body;
+
+    const errors = [];
+    if (title && typeof title !== "string") errors.push("Invalid title type");
+    if (picURL && typeof picURL !== "string")
+        errors.push("Invalid picURL type");
+    if (price && Number.isFinite(price)) errors.push("Invalid price type");
+    if (servingSize && typeof servingSize !== "string")
+        errors.push("Invalid servingSize type");
+    if (servingType && typeof servingType !== "string")
+        errors.push("Invalid servingType type");
+    if (cuisineType) {
+        cuisineType = JSON.parse(cuisineType);
+        if (!isArrayOfStrings(cuisineType))
+            errors.push("Invlalid cuisineType type");
+    }
+    if (ingredients && typeof ingredients !== "string")
+        errors.push("Invalid ingredients type");
+    if (requirements && typeof requirements !== "string")
+        errors.push("Invalid requirements type");
+
+    if (errors.length > 0) {
+        res.status(400).json({ errors });
+        return;
+    }
+
+    // if no chefId is provided, use the user's chef profile
+    if (!chefId) {
+        const userId = req.user.id;
+        const { _id } = await chefsController.findOneWithUserId(userId);
+        body.chefId = String(_id);
+    }
+
+    next();
+}
 
 module.exports = router;
