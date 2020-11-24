05b3dacf2f14f602104913156a9109484795605f
Sagar Suri <sagar_13@live.com>
2020-11-23 23:06:10 -0500

    return both chefs and meals

--- server/routes/search.js
+++ server/routes/search.js
@@ -17,13 +17,20 @@ router.get("/", async (req, res) => {
 
         const query = req.query.cuisine ?  {cuisineSpecialty: {$in: req.query.cuisine.split(',')}} : {}
         chefs = await chefsController.findAllChef(query);
+        chefs.forEach(chef => chef["meals"] = mealsController.findAllMeals( { chefId: chef._id }));
         res.json(chefs);
     }
     if (req.query.searchType === "meals"){
 
         const query = req.query.cuisine ?  {cuisineType: {$in: req.query.cuisine.split(',')}} : {}
         meals = await mealsController.findAllMeals(query);
-        res.json(meals);
+        const chefs = {};
+        meals.forEach(meal => {
+            let chef = chefsController.findOneWithId(meal.chefId);
+            chefs[chef._id] = chef;
+        })
+        
+        res.json({meals: meals, chefs});
     }
     return;
     
