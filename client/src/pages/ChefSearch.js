0a88939b6f5ca92ec6449363f8ee39c28d95f35c
gary <gsb.gurpreet@gmail.com>
2020-11-26 15:04:05 -0500

    Add mock UI for ChefSearch

--- client/src/pages/ChefSearch.js
+++ client/src/pages/ChefSearch.js
@@ -1,25 +1,55 @@
 import React from "react";
 import ResponsiveSideBar from "../components/ResponsiveSideBar";
 import Main from "../components/Main";
-import { Typography } from "@material-ui/core";
+import { Typography, Input, Grid, Box, TextField } from "@material-ui/core";
 import { UserContext } from "../contexts/user/UserContextProvider";
 
 function ChefSearch(props) {
     const { profile } = React.useContext(UserContext);
-    const { primaryAddress: { city, region, country } } = profile;
-    const location = [city, region, country].join(', ');
+    const {
+        primaryAddress: { city, region, country },
+    } = profile;
+    const userLocation = [city, region, country].join(", ");
+    const [location, setLocation] = React.useState(userLocation);
+    const [radiusKm, setRadiusKm] = React.useState(100);
+
+    const handleChangeLocation = (e) => {
+        // show suggestions using the auggestion API
+    };
+
+    const handleChangeRadiusKm = (e) => {
+        // update redius
+    };
 
     return (
         <React.Fragment>
             <ResponsiveSideBar>
-                <Typography>LOCATION:</Typography>
-                <Typography>{location}</Typography>
-                <Typography>RADIUS:</Typography>
-                <Typography>{'100km'}</Typography>
+                <Grid item xs={12} container spacing={2} alignContent="flex-start">
+                    <Grid item xs={12}>
+                        <Typography>Location:</Typography>
+                        <TextField
+                            value={location}
+                            fullWidth
+                            variant="outlined"
+                            type="text"
+                            onChange={handleChangeLocation}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography>Radius in Km:</Typography>
+                        <TextField
+                            value={radiusKm}
+                            fullWidth
+                            variant="outlined"
+                            type="number"
+                            onChange={handleChangeRadiusKm}
+                        />
+                    </Grid>
+                </Grid>
             </ResponsiveSideBar>
             <Main>
                 <Typography>Available Chefs:</Typography>
-                {'[Array of chefs]'}
+                {"[Array of chefs]"}
             </Main>
         </React.Fragment>
     );
