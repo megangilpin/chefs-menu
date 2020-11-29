4d8372439cb4815b24aa7a44b51fe9529e669e83
gary <gsb.gurpreet@gmail.com>
2020-11-29 16:30:49 -0500

    Show cuisines on chefCards and make the page responsive

--- client/src/components/ChefCard.js
+++ client/src/components/ChefCard.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 import chef1 from "../images/chef1.png";
@@ -16,6 +15,13 @@ const useStyles = makeStyles({
     },
     media: {
         height: 200,
+    },
+    name: {
+        textAlign: "center",
+        fontWeight: "bold",
+    },
+    location: {
+        textAlign: "center",
     },
 });
 
@@ -35,13 +41,26 @@ function ChefCard(props) {
     console.log(cuisineSpecialty, distanceKm, location, bio);
 
     return (
-        <Grid item xs={12} sm={6} lg={4}>
+        <Grid item xs={12} md={6} lg={4}>
             <Card>
                 <CardMedia className={classes.media} image={chef1} />
                 <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
+                    <Typography className={classes.name} gutterBottom>
                         {firstName + " " + lastName}
                     </Typography>
+                    <Typography className={classes.location} gutterBottom>
+                        {location}
+                    </Typography>
+                    {cuisineSpecialty &&
+                        cuisineSpecialty.map((cuisine) => (
+                            <Button
+                                key={cuisine}
+                                color="primary"
+                                variant="contained"
+                            >
+                                <Typography variant="button">{cuisine}</Typography>
+                            </Button>
+                        ))}
                     <Typography variant="body2" color="textSecondary" component="p">
                         {bio}
                     </Typography>
