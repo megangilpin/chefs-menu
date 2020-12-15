0a24686192a8c22db08c51bd175773a99709d616
Megan Gilpin <megangilpin@gmail.com>
2020-12-14 21:20:39 -0500

    added listen

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ const { json, urlencoded } = express;
 
 const app = express();
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "client", "build")));
+}
+
 app.use(logger("dev"));
 app.use(json());
 app.use(urlencoded({ extended: false }));
@@ -45,19 +49,9 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client", "build")));
-}
-
 // error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.json({ error: err });
+app.listen(PORT, () => {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
 
 module.exports = app;
