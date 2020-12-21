64770e310eb82730b16c58e13758396d3412e15d
Megan Gilpin <megangilpin@gmail.com>
2020-12-21 13:58:54 -0500

    fixed staic

--- app.js
+++ app.js
@@ -46,8 +46,8 @@ app.use(function (req, res, next) {
     next(createError(404));
 });
 
-app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 // error handler
